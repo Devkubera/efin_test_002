afd83e86e1c073b909c26d01de439e3cef1b3a1c
The journey dev <kittipongpiada@gmail.com>
2024-06-12 17:54:47 +0700

    complete test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,11 +23,23 @@ onMounted(async () => {
 
 <template>
   <HomeLayout>
-    <div class="flex gap-4 bg-slate-200 text-center">
-      <div v-for="(item, index) in apiData.data" :key="index">
-        {{ item }}
+    <div class="flex p-4 gap-4 text-white text-center">
+      <!-- left -->
+      <div class="flex-grow">
+        <div class="grid gap-4 grid-cols-3 h-screen">
+          <div class="bg-stone-600 row-span-2 col-span-2">
+            {{ apiData.data.data_1 }}
+          </div>
+          <div class="bg-slate-800 row-span-2">{{ apiData.data.data_2 }}</div>
+          <div class="bg-zinc-500 col-span-2">{{ apiData.data.data_3 }}</div>
+          <div class="bg-sky-950">{{ apiData.data.data_4 }}</div>
+          <div class="bg-yellow-950 col-span-full">
+            {{ apiData.data.data_5 }}
+          </div>
+        </div>
       </div>
+      <!-- right -->
+      <div class="flex-grow bg-violet-950">{{ apiData.data.data_6 }}</div>
     </div>
   </HomeLayout>
 </template>
-
